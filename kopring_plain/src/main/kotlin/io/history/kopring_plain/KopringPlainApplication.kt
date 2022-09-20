8b891438b50731ff3f17abdcb2a17f0809e10231
soon.good <soon.good@outlook.com>
2022-09-20 17:33:48 +0900

    kopring 예제 기록용도

---
+++ kopring_plain/src/main/kotlin/io/history/kopring_plain/KopringPlainApplication.kt
@@ -0,0 +1,11 @@
+package io.history.kopring_plain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringPlainApplication
+
+fun main(args: Array<String>) {
+	runApplication<KopringPlainApplication>(*args)
+}
---
+++ kopring_plain/src/test/kotlin/io/history/kopring_plain/KopringPlainApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.history.kopring_plain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringPlainApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
