ebe20c155c39d646a246abaf2c503ea1efd98596
soon.good <soon.good@outlook.com>
2022-10-04 21:38:20 +0900

    코드 insert 스크립트 추가

--- 뒤죽박죽-천천히-정리해가는-내용들/springwebflux-kopring-202209/kopring_webflux_202209/src/main/resources/mysql-schema.sql
+++ 뒤죽박죽-천천히-정리해가는-내용들/springwebflux-kopring-202209/kopring_webflux_202209/src/main/resources/mysql-schema.sql
@@ -1,8 +1,7 @@
 -- https://stackoverflow.com/questions/13357760/mysql-create-user-if-not-exists
-GRANT ALL ON `collector`.* TO 'collector'@'localhost' IDENTIFIED BY '1111'
+-- GRANT ALL ON `collector`.* TO 'collector'@'localhost' IDENTIFIED BY '1111'
 -- GRANT ALL ON `collector`.* TO 'collector'@'%' IDENTIFIED BY '1111'
 
--- CREATE DATABASE IF NOT EXISTS collector character set utf8mb4 collate utf8mb4_general_ci;
 -- CREATE DATABASE IF NOT EXISTS shop character set utf8mb4 collate utf8mb4_general_ci;
 CREATE DATABASE IF NOT EXISTS collector character set utf8mb4 collate utf8mb4_general_ci;
 
@@ -23,7 +22,15 @@ CREATE TABLE IF NOT EXISTS book
 
 CREATE TABLE IF NOT EXISTS book_category
 (
-    code varchar(10) not null
+    code     varchar(10) not null
         primary key,
-    name varchar(50) null
+    name     varchar(50) null,
+    label_kr varchar(50) null
 );
+
+INSERT INTO collector.book_category (code, name, label_kr) VALUES ('FANTASY', 'FANTASY', '판타지');
+INSERT INTO collector.book_category (code, name, label_kr) VALUES ('IT_MOBILE', 'IT_MOBILE', 'IT/모바일');
+INSERT INTO collector.book_category (code, name, label_kr) VALUES ('LITERATURE', 'LITERATURE', '문학');
+INSERT INTO collector.book_category (code, name, label_kr) VALUES ('SCIENCE', 'SCIENCE', '과학');
+
+commit;
