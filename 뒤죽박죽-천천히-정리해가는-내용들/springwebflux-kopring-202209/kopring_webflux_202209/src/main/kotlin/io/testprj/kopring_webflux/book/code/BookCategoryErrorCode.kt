f20f0f74f610c21c30dd143827abdbcb7529a995
soon.good <soon.good@outlook.com>
2022-10-05 23:18:44 +0900

    예제용도로 아주 오랫적에 작성했던 코드들 제거

--- 뒤죽박죽-천천히-정리해가는-내용들/springwebflux-kopring-202209/kopring_webflux_202209/src/main/kotlin/io/testprj/kopring_webflux/book/code/BookCategoryErrorCode.kt
+++
@@ -1,9 +0,0 @@
-package io.testprj.kopring_webflux.book.code
-
-enum class BookCategoryErrorCode (
-    val errorCode: Int,
-    val errorMsg: String,
-    val errorDetail: String
-){
-    NO_SUCH_CATEGORY_EXISTS(50001, "NO_SUCH_CATEGORY_EXISTS", "존재하지 않는 카테고리입니다.")
-}
