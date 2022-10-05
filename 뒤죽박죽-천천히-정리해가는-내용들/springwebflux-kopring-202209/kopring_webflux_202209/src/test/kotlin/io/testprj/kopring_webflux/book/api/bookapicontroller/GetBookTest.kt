727166b65cd4707a3236baab36aaafd6101f4fad
soon.good <soon.good@outlook.com>
2022-10-05 09:17:20 +0900

    GetBookTest 삭제

--- 뒤죽박죽-천천히-정리해가는-내용들/springwebflux-kopring-202209/kopring_webflux_202209/src/test/kotlin/io/testprj/kopring_webflux/book/api/bookapicontroller/GetBookTest.kt
+++ 뒤죽박죽-천천히-정리해가는-내용들/springwebflux-kopring-202209/kopring_webflux_202209/src/test/kotlin/io/testprj/kopring_webflux/book/api/bookapicontroller/GetBookTest.kt
@@ -3,16 +3,11 @@ package io.testprj.kopring_webflux.book.api.bookapicontroller
 import io.testprj.kopring_webflux.book.api.BookApiController
 import io.testprj.kopring_webflux.book.application.BookNameChecker
 import io.testprj.kopring_webflux.config.GlobalExceptionHandlerConfig
-import io.testprj.kopring_webflux.global.code.ErrorCode
-import io.testprj.kopring_webflux.global.exception.ErrorResponse
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.FilterType
-import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 
@@ -30,23 +25,23 @@ class GetBookTest {
     @Autowired
     lateinit var webTestClient: WebTestClient
 
-    @Test
-    fun `'name에 '보라빛향기'가 있다면 예외 발생'`(){
-        val body = webTestClient.get()
-            .uri {
-                it.path("/book")
-                    .queryParam("name", "보라빛향기")
-                    .build()
-            }
-            .accept(MediaType.APPLICATION_JSON)
-            .exchange()
-            .expectStatus().is2xxSuccessful()
-            .expectBody(ErrorResponse::class.java)
-
-        Assertions
-            .assertThat(body.returnResult().responseBody?.errorMessage)
-            .isEqualTo(ErrorCode.THAT_IS_NOT_A_BOOK.errorMessage)
-
-        println("ErrorResponse >>> ${body.returnResult().responseBody?.errorMessage}")
-    }
+//    @Disabled
+//    fun `'name에 '보라빛향기'가 있다면 예외 발생'`(){
+//        val body = webTestClient.get()
+//            .uri {
+//                it.path("/book")
+//                    .queryParam("name", "보라빛향기")
+//                    .build()
+//            }
+//            .accept(MediaType.APPLICATION_JSON)
+//            .exchange()
+//            .expectStatus().is2xxSuccessful()
+//            .expectBody(ErrorResponse::class.java)
+//
+//        Assertions
+//            .assertThat(body.returnResult().responseBody?.errorMessage)
+//            .isEqualTo(ErrorCode.THAT_IS_NOT_A_BOOK.errorMessage)
+//
+//        println("ErrorResponse >>> ${body.returnResult().responseBody?.errorMessage}")
+//    }
 }
