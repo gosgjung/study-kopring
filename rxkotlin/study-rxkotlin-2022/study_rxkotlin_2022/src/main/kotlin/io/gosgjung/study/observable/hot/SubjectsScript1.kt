b4049adfb77a2fafb1d51d6d476260088ae5d5f9
gosgjung <gosgjung@gmail.com>
2022-12-11 07:22:27 +0900

    Subject 예제

--- rxkotlin/study-rxkotlin-2022/study_rxkotlin_2022/src/main/kotlin/io/gosgjung/study/observable/hot/SubjectsScript1.kt
+++ rxkotlin/study-rxkotlin-2022/study_rxkotlin_2022/src/main/kotlin/io/gosgjung/study/observable/hot/SubjectsScript1.kt
@@ -1,4 +1,31 @@
 package io.gosgjung.study.observable.hot
 
-class SubjectsScript1 {
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.PublishSubject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
+
+fun main(args: Array<String>) {
+    // (1) 100ms 간격으로 옵저버블 인스턴스를 다시 생성
+    val observable = Observable.interval(100, TimeUnit.MILLISECONDS)
+
+    // (2) PublishSubject.create 로 Subject 를 생성
+    val subject = PublishSubject.create<Long>()
+
+    // (3) Subject 인스턴스를 사용해 옵저버블 인스턴스의 배출을 구독
+    observable.subscribe(subject)
+
+    // (4) Subject 인스턴스를 사용해 Subject 인스턴스에 의한 배출에 접근하기 위해 람다를 사용해 구독
+    // 마치 옵저버블처럼 구독을 하고 있다.
+    subject.subscribe(
+        { println("Received $it") }
+    )
+
+    // (5) 1100 밀리초 대기
+    // 모든 코드 실행이 완료될 때 까지 runBlocking이 스레드를 차단하면서
+    // 호출 스레드 내부에서 코루틴 컨텍스트를 모킹(Mocking, 흉내)한다.
+    runBlocking { delay(1100) }
 }
+
+
--- rxkotlin/study-rxkotlin-2022/study_rxkotlin_2022/src/main/kotlin/io/gosgjung/study/observable/hot/SubjectsScript2.kt
+++ rxkotlin/study-rxkotlin-2022/study_rxkotlin_2022/src/main/kotlin/io/gosgjung/study/observable/hot/SubjectsScript2.kt
@@ -1,4 +1,37 @@
 package io.gosgjung.study.observable.hot
 
-class SubjectsScript2 {
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.PublishSubject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
+
+fun main(args: Array<String>){
+    // (1) 100 밀리초 간격으로 옵저버블 인스턴스를 생성한다.
+    val observable = Observable.interval(100, TimeUnit.MILLISECONDS)
+
+    // (2) PublishSubject.create() 로 Subject 를 생성했다.
+    val subject = PublishSubject.create<Long>()
+
+    // (3) 옵저버처럼 Subject 인스턴스를 사용해 옵저버블 인스턴스의 배출을 구독
+    observable.subscribe(subject)
+
+    // (4) 옵저버블 처럼 Subject 인스턴스를 사용해 Subject 인스턴스에 의한 배출에 접근하기 위해 람다를 사용해 구독
+    subject.subscribe(
+        { println("Subscription 1 Received $it") }
+    )
+
+    // (5)
+    runBlocking{
+        delay(1100)
+    }
+
+    // (6) Subject 를 다시 구독
+    // 1100 밀리초 후에 구독을 한다. 따라서 처음 11회의 배출 이후의 데이터를 받는다.
+    subject.subscribe(
+        { println("Subscription 2 Receive $it") }
+    )
+
+    // (7) 다시 프로그램을 1100 밀리초 기다리게 한다.
+    runBlocking { delay(1100) }
 }
