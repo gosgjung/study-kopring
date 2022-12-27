4f6299d00ae304b69f9b648de8a151f5f2b4f561
gosgjung <gosgjung@gmail.com>
2022-12-27 16:09:57 +0900

    초기커밋

---
+++ kopring+webflux/examples/kopring-multimodule/src/main/kotlin/io/kopring/multimodule/kopringmultimodule/KopringMultimoduleApplication.kt
@@ -0,0 +1,11 @@
+package io.kopring.multimodule.kopringmultimodule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringMultimoduleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringMultimoduleApplication>(*args)
+}
---
+++ kopring+webflux/examples/kopring-multimodule/src/test/kotlin/io/kopring/multimodule/kopringmultimodule/KopringMultimoduleApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.kopring.multimodule.kopringmultimodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringMultimoduleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
