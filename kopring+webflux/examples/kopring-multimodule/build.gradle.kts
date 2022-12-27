9597547a659018bc632d1322fe841097b14ca12d
gosgjung <gosgjung@gmail.com>
2022-12-27 17:14:04 +0900

    코프링 멀티모듈 구성방식 예제 추가

--- kopring+webflux/examples/kopring-multimodule/build.gradle.kts
+++ kopring+webflux/examples/kopring-multimodule/build.gradle.kts
@@ -1,35 +1,57 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.7"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    // kotlin
     kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
+
+    // spring
+    id("io.spring.dependency-management") version "1.0.15.RELEASE" apply false
+    kotlin("plugin.spring") version "1.6.21" apply false
+    id("org.springframework.boot") version "2.7.7" apply false
 }
 
-group = "io.kopring.multimodule"
-version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
-repositories {
-    mavenCentral()
-}
+allprojects{
+    group = "io.kopring.multimodule"
+    version = "0.0.1-SNAPSHOT"
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+    repositories {
+        mavenCentral()
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+subprojects{
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+
+    dependencies {
+//        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    tasks.getByName("bootJar"){
+        enabled = false
+    }
+
+    tasks.getByName("jar"){
+        enabled = true
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
+
--- kopring+webflux/examples/kopring-multimodule/settings.gradle.kts
+++ kopring+webflux/examples/kopring-multimodule/settings.gradle.kts
@@ -1,1 +1,4 @@
 rootProject.name = "kopring-multimodule"
+include(
+    "kopring-api"
+)
