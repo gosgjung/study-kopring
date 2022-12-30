3aa9d043c2c5526ee9c7504841613864fcfe3b14
gosgjung <gosgjung@gmail.com>
2022-12-31 01:46:57 +0900

    FP 만 따로 공부하는 용도의 프로젝트 추가 = 굳이 SpringBoot 기반으로 한것은 매번 Gradle 의존성 추가하기 귀찮은 면도 있어서

---
+++ functional-programming/examples/fp-study/src/main/kotlin/io/gosgjung/study/fpstudy/FpStudyApplication.kt
@@ -0,0 +1,11 @@
+package io.gosgjung.study.fpstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FpStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<FpStudyApplication>(*args)
+}
