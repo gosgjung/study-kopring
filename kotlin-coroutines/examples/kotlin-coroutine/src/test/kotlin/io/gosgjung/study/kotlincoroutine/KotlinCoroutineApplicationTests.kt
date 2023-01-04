79b5c8a441bc0ee6a9cc2817a9c8bad177432e4c
gosgjung <gosgjung@gmail.com>
2023-01-04 17:29:18 +0900

    예제 프로젝트 초기구성

---
+++ kotlin-coroutines/examples/kotlin-coroutine/src/test/kotlin/io/gosgjung/study/kotlincoroutine/KotlinCoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.gosgjung.study.kotlincoroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCoroutineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
