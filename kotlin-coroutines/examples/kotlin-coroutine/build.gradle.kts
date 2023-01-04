8d3cbc3637bd5cdc4310c14307d0583e19fa1e4e
gosgjung <gosgjung@gmail.com>
2023-01-04 17:37:00 +0900

    코루틴 의존성 추가

--- kotlin-coroutines/examples/kotlin-coroutine/build.gradle.kts
+++ kotlin-coroutines/examples/kotlin-coroutine/build.gradle.kts
@@ -19,6 +19,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
